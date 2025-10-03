c464aa74873d4929ef9a9ec73bcc8f553387c0b5
Eric Dala Porta <ericdasilvadalaporta@gmail.com>
2025-10-03 19:43:10 -0300

    Add files via upload

---
+++ src/app/components/services/listar-servico/listar-servico.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Servico } from '../../../models/servico.model';
+import { ServicoService } from '../../../services/servico.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-listar-servico',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './listar-servico.component.html'
+})
+export class ListarServicosComponent implements OnInit {
+  servicos: Servico[] = [];
+
+  constructor(
+    private servicoService: ServicoService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.getAllServicos();
+  }
+
+  getAllServicos() {
+    this.servicoService.getAllServicos().then(servicos => {
+      this.servicos = servicos;
+    });
+  }
+
+  trackById(index: number, item: Servico) {
+    return item.id;
+  }
+
+  editarServico(id: number) {
+    this.router.navigate(['/servicos/editar-servico', id]);
+  }
+
+  excluirServico(id: number) {
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Esta ação não pode ser desfeita!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, excluir!',
+      cancelButtonText: 'Cancelar'
+    }).then(result => {
+      if (result.isConfirmed) {
+        this.servicoService.deleteServico(id).then(() => {
+          Swal.fire('Excluído!', 'O serviço foi excluído com sucesso.', 'success');
+          this.getAllServicos();
+        });
+      }
+    });
+  }
+}
